b2ba3eca7681dd3210e0b62e3794a03db04aa777
felipefrancca <suporte.felipefranca@gmail.com>
2023-11-11 17:07:20 -0300

    starting

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,173 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import "../../assets/styles/Dashboard.css";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+import AppsOutageIcon from "@mui/icons-material/AppsOutage";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+const pages = ["Worlspaces", "Recent", "Starred", "Templates"];
+const account = [
+  "Switch accounts",
+  "Manage accounts",
+  "Profile and visibility",
+  "Activity",
+  "Cards",
+  "Settings",
+  "Theme",
+  "Help",
+  "Shortcuts",
+  "Log out",
+];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar className="appBar" style={{ backgroundColor: "#1D2125" }}>
+      <Box style={{ padding: "0 1rem 0 1rem" }}>
+        <Toolbar disableGutters>
+          <AppsOutageIcon />
+          <Box className="imageLogoContainer">
+            <Box className="imageLogo"></Box>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ display: "flex !important", alignItems: 'center',  gap: 1 }}>
+            <NotificationsIcon />
+            <HelpOutlineIcon />
+            <Tooltip
+              title="Account"
+              sx={{ display: "flex !important", gap: 8 }}
+            >
+              <IconButton onClick={handleOpenUserMenu}>
+                <Avatar
+                  alt="Felipe"
+                  src="/static/images/avatar/2.jpg"
+                  style={{ width: 24, height: 24, fontSize: 14 }}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {account.map((account) => (
+                <MenuItem key={account} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{account}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Box>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
---
+++ src/pages/dashboard/views/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box} from "@mui/material";
+import "../../../assets/styles/home.css";
+
+export default function Home() {
+  return (
+    <Box className="homeConteiner">
+      
+    </Box>
+  );
+}
