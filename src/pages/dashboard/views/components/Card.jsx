fc557dec75fa826c8f73c331032db2649b8044b0
felipefrancca <suporte.felipefranca@gmail.com>
2023-11-25 11:43:15 -0300

    up

--- src/pages/dashboard/views/components/Card.jsx
+++ src/pages/dashboard/views/components/Card.jsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     card: {
       padding: theme.spacing(1),
-      margin: theme.spacing(1)
+      margin: theme.spacing(1),
     },
   }));
 
